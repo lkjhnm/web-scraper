5e6604cbf101d06de26ae56de37e9f0f56960cd2
daecheol.choi <cdc9308@naver.com>
2022-04-30 16:37:55 +0900

    refact: HTML(테스트용) 생성 로직 setUp 메서드로 이관

--- tests/test_data_scraper.py
+++ tests/test_data_scraper.py
@@ -9,19 +9,20 @@ from utils import path_util
 class DataScraperTestCase(unittest.TestCase):
 	target_url = 'https://search.shopping.naver.com/search/all?query=RTX+3060TI&bt=-1&frm=NVSCPRO'
 
+	def setUp(self):
+		test_resources = path_util.ROOT_DIR.joinpath("tests").joinpath("resources").joinpath("test.html")
+		test_file = open(f'{test_resources}', 'r', encoding='UTF-8')
+		self.__test_html__ = ''
+		for line in test_file.readlines():
+			self.__test_html__ += line
+		test_file.close()
+
 	def test_download(self):
 		html_text = data_scraper.download(DataScraperTestCase.target_url)
 		self.assertIsNotNone(html_text)
 
 	def test_parse(self):
-		test_resources = path_util.ROOT_DIR.joinpath("tests").joinpath("resources").joinpath("test.html")
-		test_file = open(f'{test_resources}', 'r', encoding='UTF-8')
-		data = ''
-		for line in test_file.readlines():
-			data += line
-		test_file.close()
-
-		product_infos = data_scraper.parse(data)
+		product_infos = data_scraper.parse(self.__test_html__)
 		self.assertIsNotNone(product_infos)
 
 
